ff6fb4dbaa85b916d7672cbbdeec812ed6d0291a
Welbert Gomes <92159326+WelGomes@users.noreply.github.com>
2023-07-25 09:00:57 -0300

    header.php

--- Projeto Blog/templates/header.php
+++ Projeto Blog/templates/header.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= $BASE_URL?>/css/style.css">
+    <link rel="stylesheet" href="<?php $BASE_URL?>/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Oswald:wght@200&display=swap" rel="stylesheet">
@@ -18,15 +18,15 @@
 </head>
 <body>
     <header>
-        <a href="<?= $BASE_URL ?>" id="logo"> 
-            <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
+        <a href="<?php $BASE_URL ?>" id="logo"> 
+            <img src="<?php $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
         </a>
         <nav>
             <ul id="navbar">
-                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="<?php $BASE_URL ?>" class="nav-link">Home</a></li>
                 <li><a href="#" class="nav-link">Categorias</a></li>
                 <li><a href="#" class="nav-link">Sobre</a></li>
-                <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
+                <li><a href="<?php $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
             </ul>
         </nav>
     </header>
